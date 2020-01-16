139ec0116ed0ecfb6305d4a1c20e97532e18023e
prasanth <prasanth@prasanth-PC>
2020-01-16 19:18:19 +0530

    FreshersProgsBasics

---
+++ DataStructures/src/com/prasanth/javaBasics/BasicLogicProgs.java
@@ -0,0 +1,96 @@
+package com.prasanth.javaBasics;
+
+public class BasicLogicProgs {
+
+	static String strInput = "jim cary is Awsome actor";
+
+	public static boolean ispalindrome(String input) {
+		int size = input.length() - 1;
+		for (int i = 0; i < size; i++) {
+			if (input.charAt(i) != input.charAt(size - i)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static int reverseANumber(int data) {
+		if (data < 10) {
+			return data;
+		}
+		int temp = 0;
+		while (data > 0) {
+			temp = temp * 10 + data % 10;
+			data = data / 10;
+		}
+		return temp;
+	}
+
+	public static String reverseString(String str) {
+		char[] arr = str.toCharArray();
+		int size = str.length() - 1;
+		char temp;
+
+		for (int i = 0; i < arr.length / 2; i++) {
+			temp = arr[i];
+			arr[i] = arr[size - i];
+			arr[size - i] = temp;
+		}
+		return new String(arr);
+	}
+
+	private static boolean isPrime(int num) {
+		if (num % 2 == 0) {
+			return false;
+		}
+		for (int i = 3; i < num; i = i + 2) {
+			if (num % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private static void printFibbnociSeries(int n) {
+		int a = 0;
+		int b = 0;
+		int c = 1;
+		while (a < n) {
+			a = b;
+			b = c;
+			c = a + b;
+			System.out.println(a + "");
+		}
+
+	}
+
+	private static String reverseWordsinString(String strInput2) {
+		String[] data = strInput2.split(" ");
+		strInput2 = "";
+		for (int i = 0; i < data.length; i++) {
+			strInput2 += reverseString(data[i]) + " ";
+		}
+		return strInput2;
+	}
+
+	public static Integer getPossibleIndex(int[] data, int index, int element) {
+		while (data[index] < element && index++ < data.length - 1)
+			;
+		return index;
+	}
+
+	public static void main(String[] args) {
+
+		int[] data = { 1, 3, 4, 5, 7, 9 };
+		System.out.println(getPossibleIndex(data, 0, 11));
+		for (int i = 2; i < 100; i++) {
+			System.out.println("is " + i + " prime ::" + isPrime(i));
+		}
+		System.out.println(ispalindrome("Amrutha"));
+		System.out.println(reverseString("TalkWise"));
+		System.out.println(reverseANumber(1234));
+		printFibbnociSeries(123);
+		System.out.println(reverseWordsinString(strInput));
+	}
+
+}
---
+++ DataStructures/src/com/prasanth/javaBasics/HighestOfGivenProducts.java
@@ -0,0 +1,28 @@
+package com.prasanth.javaBasics;
+
+public class HighestOfGivenProducts {
+
+	
+	
+	public static void main(String[] args) {
+		int[] arr1={4,1,7,3,9,11,5};
+		int[] arr2={6,11,23,5,7,1,0};
+		int[] arr3={12,3,6,2,3,8,10};
+		
+		System.out.println("max income of arr1 \t"+findMaxIncome(arr1));
+		System.out.println("max income of arr2 \t"+findMaxIncome(arr2));
+		System.out.println("max income of arr3 \t"+findMaxIncome(arr3));
+	}
+
+	private static int findMaxIncome(int[] arr) {
+		int a=arr[0];
+		for(int i=1;i<arr.length;i++)
+		{
+			if(arr[i]>a)
+			{
+				a=arr[i];
+			}
+		}
+		return a;
+	}
+}
